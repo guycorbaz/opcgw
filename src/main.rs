8b87bd0a68b9070238d19de537d7b187a3702325
Guy <guy@corbaz.org>
2024-09-30 13:14:08 +0200

    feat: Add logging and load configuration

--- src/main.rs
+++ src/main.rs
@@ -19,26 +19,27 @@ use log::{info, warn, error, debug};
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Configurer le logger
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap();
+    info!("Starting opc ua chirpstack gateway");
 
     // Charger la configuration
-    debug!("Load configuration");
+    //debug!("Load configuration");
     let config = AppConfig::new().expect("Failed to load configuration");
     
     info!("ChirpStack to OPC UA Gateway");
     info!("ChirpStack server: {}", config.chirpstack.server_address);
-    info!("OPC UA server: {}", config.opcua.server_url);
+    //info!("OPC UA server: {}", config.opcua.server_url); TODO: uncoment
+    //info!("OPC UA server name: {}", config.opcua.server_name); TODO: uncoment
 
     // Initialiser les composants
-    debug!("Initialise chirpstack client");
     let chirpstack_client = ChirpstackClient::new(config.chirpstack).await?;
-    debug!("Initialise opc ua server");
-    let opc_ua_server = OpcUaServer::new(config.opcua);
-    debug!("Initialise storage");
-    let (storage, command_receiver) = Storage::new();
+    let applications = chirpstack_client.list_applications();
+    chirpstack::print_list(&applications); //TODO: remove: for debugging purpose
+    //let opc_ua_server = OpcUaServer::new(config.opcua); TODO: uncoment
+    //let (storage, command_receiver) = Storage::new(); TODO: uncoment
 
     // Démarrer le serveur OPC UA
-    debug!("Start OPC UA server");
-    opc_ua_server.start()?;
+    //debug!("Start OPC UA server");
+    //opc_ua_server.start()?; TODO:uncoment
 
     // Ici, nous ajouterons la logique principale de l'application
     // Par exemple, une boucle pour traiter les commandes et les données
